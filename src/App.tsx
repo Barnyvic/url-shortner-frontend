dcd73deaae50653d2ccee295395234f5989c51d9
Barny Victor <victorbarny4@gmail.com>
2023-06-21 15:14:03 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,6 @@
-import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import Layout from "./components/layout";
-import Login from "./components/login";
-import SignUp from "./components/signup";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
   return (
     <div>
       <h1>hello world </h1>
